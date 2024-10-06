386174dd0c5a68055234dfd439d017ea43f00d9e
Victor Bauer <dhorthe@gmail.com>
2024-10-06 09:54:52 -0400

    build: adição do Lombok para uso da geração de logs de forma refinada.

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,12 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.4.0")
+	compileOnly("org.projectlombok:lombok:1.18.34")
+	annotationProcessor("org.projectlombok:lombok:1.18.34")
+	testCompileOnly("org.projectlombok:lombok:1.18.34")
+	testAnnotationProcessor("org.projectlombok:lombok:1.18.34")
+	implementation("ch.qos.logback:logback-core:1.4.14")
+	implementation("ch.qos.logback:logback-classic:1.4.12")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
